5dd733a4dd092ce7730505f0b019015db8671806
Enkosi Xalisa <enkosi.xalisa@league.co.za>
2021-05-28 10:08:36 +0200

    Initial commit

---
+++ DSO34BT/DSO34BT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DSO34BT.MvcApplication" Language="C#" %>
---
+++ DSO34BT/DSO34BT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DSO34BT.Startup))]
+namespace DSO34BT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
